e4ed9d910b932cc26c23a9b27def860df936d7cd
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-20 14:19:36 +0200

    i18n: add missing FR translations

--- i18n_fr.lua
+++ i18n_fr.lua
@@ -2,4 +2,8 @@ return {
     ["Found: %1!"]                        = { fr = "Trouvé : %1 !" },
     ["Found: %1/%2 words"]                = { fr = "Trouvé : %1/%2 mots" },
     ["No match. Try again."]              = { fr = "Pas de correspondance. Réessayez." },
+    ["Congratulations! All words found!"]     = { fr = "Félicitations ! Tous les mots sont trouvés !" },
+    ["All words found!"]                      = { fr = "Tous les mots trouvés !" },
+    ["Find hidden words in a letter grid"]    = { fr = "Trouvez des mots cachés dans une grille de lettres" },
+    ["Word Search"]                           = { fr = "Mots mêlés" },
 }
