bfab11cd991fba85d0866fd7b31977686ea33c3b
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-21 16:28:45 +0200

    chore: bump version to 1.1.7

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name        = "wordsearch",
     fullname    = _("Word Search"),
     description = _("Find hidden words in a letter grid"),
-    version     = "1.1.6",
+    version     = "1.1.7",
 }
